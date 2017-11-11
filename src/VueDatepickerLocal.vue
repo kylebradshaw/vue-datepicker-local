e5a3e85cca04a85ac0f635e89ac22c51378588fe
Sandeep Ramgolam <s4ndeep1203@gmail.com>
2017-11-12 03:58:36 +0400

    updated Locale to default to french

--- src/VueDatepickerLocal.vue
+++ src/VueDatepickerLocal.vue
@@ -50,14 +50,14 @@ export default {
       type: Object,
       default () {
         return {
-          dow: 1, // Monday is the first day of the week
-          hourTip: '选择小时', // tip of select hour
-          minuteTip: '选择分钟', // tip of select minute
-          secondTip: '选择秒数', // tip of select second
-          yearSuffix: '年', // format of head
-          monthsHead: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'), // months of head
-          months: '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'), // months of panel
-          weeks: '一_二_三_四_五_六_日'.split('_') // weeks
+          dow: 1, // Sunday is the first day of the week
+          hourTip: 'Select Hour', // tip of select hour
+          minuteTip: 'Select Minute', // tip of select minute
+          secondTip: 'Select Second', // tip of select second
+          yearSuffix: '', // suffix of head year
+          monthsHead: 'Janvier_Fevrier_Mars_Avril_Mai_Juin_Julliet_Aout_Septembre_Octobre_Novembre_Decembre'.split('_'), // months of head
+          months: 'Jan_Fev_Mar_Avr_Mai_Jun_Jul_Aout_Sep_Oct_Nov_Dec'.split('_'), // months of panel
+          weeks: 'Dim_Lun_Mar_Mer_Jeu_Ven_Sam'.split('_') // weeks
         }
       }
     }
